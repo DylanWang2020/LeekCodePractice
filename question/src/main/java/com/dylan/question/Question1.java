3853c349eebf09b2919533f5cc71ca7206809a58
DylanWang <15257102022@126.com>
2021-04-27 14:28:22 +0800

    Add LK Question 1&2
    Signed-off-by: DylanWang <15257102022@126.com>

---
+++ question/src/main/java/com/dylan/question/Question1.java
@@ -0,0 +1,29 @@
+package com.dylan.question;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int result[] = twoSum(new int[]{1, 2, 3, 4}, 7);
+        System.out.println("" + result[0] + ", " + result[1]);
+    }
+
+    static public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int len = nums.length;
+        int j;
+        for (int i=0; i<len; ++i) {
+            j = target - nums[i];
+            if (map.containsKey(j)) {
+                return new int[]{ (int) map.get(j), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
---
+++ question/src/main/java/com/dylan/question/Question2.java
@@ -0,0 +1,52 @@
+package com.dylan.question;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        ListNode result = addTwoNumbers(new ListNode(2), new ListNode(3));
+        System.out.println("" + result.val);
+    }
+
+    static public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode list = null, temp = null;
+        int value1, value2, sum, value, after = 0;
+        while (l1 != null || l2 != null) {
+            value1 = 0;
+            if (l1 != null) {
+                value1 = l1.val;
+                l1 = l1.next;
+            }
+            value2 = 0;
+            if (l2 != null) {
+                value2 = l2.val;
+                l2 = l2.next;
+            }
+            sum = value1 + value2 + after;
+            value = sum % 10;
+            after = sum / 10;
+            if (list == null) {
+                list = new ListNode(value);
+                temp = list;
+            } else {
+                temp.next = new ListNode(value);
+                temp = temp.next;
+            }
+        }
+        if (after > 0) {
+            temp.next = new ListNode(after);
+        }
+        return list;
+    }
+
+    static public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
